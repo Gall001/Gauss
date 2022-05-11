a00b4a4a31bde9abbb2593ec847c3aa4247eebc4
Gal Levy <gallevy@edu.unifil.br>
2022-05-10 22:12:35 -0300

    ajustes

--- js.js
+++ js.js
@@ -42,9 +42,33 @@ function addTable() {
       array[i] = arrayI;
     }
     console.log(array)
+    console.log('nao chegamos nas escadas ainda')
     return array
 }
 
+
+function stairs(){
+  var array = makeMetrix()
+  const stairs = []
+  const zerado = [];
+  var stairSize= array.length
+  console.log('inicio das escadas, tamanho: '+array.length)
+  for (let i = 0; i < stairSize; i++) {
+    for (let j = 0; j < i; j++) {
+      console.log(array[i][j])
+      console.log("Antes array[i][j] "+array[i][j]+" array[i-1][j] "+array[i-1][j])
+      var M = array[i][j] / array[i-j][j]
+      console.log(M + "  esse eh o M")
+      for (let x = 0; x < stairSize+1; x++) {
+        console.log('ANTES array i x: '+ array[i][x]+' array i j: '+ array[i][j]+ ' m: '+ M +' array i-1 j: '+ array[i-1][j])
+        if(array[i][x] !=0){zerado[x] = array[i][x] = array[i][x] - M * array[i-1][j]}
+        console.log('DEPOIS array i x: '+ array[i][x]+' array i j: '+ array[i][j]+ ' m: '+ M +' array i-1 j: '+ array[i-1][j])
+      }
+      console.log(zerado);
+    }
+  }
+
+}
 
 /*
 1 2 3   4
@@ -61,53 +85,53 @@ Array B = no sistema acima o array B é:
     7
     6
 */
-const m = [ [3, 1, 5, 35,    5],   
-            [0, 2, 5, 12,   66],
-            [5, 5, 1, 8,    18],
-            [8, 6, 2, 5,    34]   ];
+// const m = [ [3, 1, 5, 35,    5],   
+//             [0, 2, 5, 12,   66],
+//             [5, 5, 1, 8,    18],
+//             [8, 6, 2, 5,    34]   ];
 
 
 
-// Formula
-function formula(multiplicador, x, pivo) {
-    this.x = x - (multiplicador * pivo)
-    return this.x
-}
-// Metodo que procura o pivo na coluna especificada, tendo como parametro o array completo e o x
-// e retorna o multiplicador
-function acharMultiplicador(array, coluna, x){
-    var multiplicador
-    for(let i = 0; i < array.length; i++) {
-        for (let j = coluna; j < array[i].length;){
-            if (array[i][j]!= 0) {
-                multiplicador = array[i][j]
-                return multiplicador
-                break
-            }else{
-                break
-            }
-        }
-        if (multiplicador) {
-            break
-        }
-    }
-}
+// // Formula
+// function formula(multiplicador, x, pivo) {
+//     this.x = x - (multiplicador * pivo)
+//     return this.x
+// }
+// // Metodo que procura o pivo na coluna especificada, tendo como parametro o array completo e o x
+// // e retorna o multiplicador
+// function acharMultiplicador(array, coluna, x){
+//     var multiplicador
+//     for(let i = 0; i < array.length; i++) {
+//         for (let j = coluna; j < array[i].length;){
+//             if (array[i][j]!= 0) {
+//                 multiplicador = array[i][j]
+//                 return multiplicador
+//                 break
+//             }else{
+//                 break
+//             }
+//         }
+//         if (multiplicador) {
+//             break
+//         }
+//     }
+// }
 
-function procurarUltimo(array){
-    for (let i = array.length-1; i > 0; i--){
-        for (let j = 0; j < array.length; j++) {
-           if (array[i][j] != 0) {
-               console.log(array[i][j])
-               break
-           }else{
-               break
-           }
+// function procurarUltimo(array){
+//     for (let i = array.length-1; i > 0; i--){
+//         for (let j = 0; j < array.length; j++) {
+//            if (array[i][j] != 0) {
+//                console.log(array[i][j])
+//                break
+//            }else{
+//                break
+//            }
             
-        }
-    }
-}
+//         }
+//     }
+// }
 
 
-var x = acharMultiplicador(m,3)
-console.log(x)
-//metodo(m)
+// var x = acharMultiplicador(m,3)
+// //console.log(x)
+// //metodo(m)
